e63fda3dad187e8a951bba43a223e60911d9fa78
Vladimir <v.r.kladov@gmail.com>
2021-01-22 18:23:02 +0500

    Jump script and basic scene

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce = 5;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private bool IsGrounded => Physics.Raycast(transform.position, Vector3.down, 0.3f);
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && IsGrounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+}
